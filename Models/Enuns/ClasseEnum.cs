d5a1144ee08777fff2c5d6630b3751193188984d
FelipeSardinhaMiguel <felipesardinham@gmail.com>
2025-08-27 22:30:46 -0300

    Aula 05

---
+++ Models/Enuns/ClasseEnum.cs
@@ -0,0 +1,12 @@
+namespace RpgAPI.Models.Enuns
+{
+    public enum ClasseEnum
+    {
+        Cavaleiro = 1,
+        Mago = 2,
+        Clerigo = 3,
+        /*Bardo = 4,
+        Monge = 5,
+        Paladino = 6*/
+    }
+}
---
+++ Models/Personagem.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RpgAPI.Models.Enuns;
+
+namespace RpgAPI.Models
+{
+    public class Personagem
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public int PontosVida { get; set; }
+        public int Forca { get; set; }
+        public int Defesa { get; set; }
+        public int Inteligencia { get; set; }
+        
+        /*public stiring Raca { get; set; } = string.Empty*/
+        
+        public ClasseEnum Classe { get; set; }
+        
+
+    }
+}
